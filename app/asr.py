2ed1f8694b912c0c502e232985def8e9749cccce
plliang <puliang@wiscred.com>
2024-07-19 11:06:28 +0800

    asr

--- app/asr.py
+++ app/asr.py
@@ -1,3 +1,5 @@
+import os
+
 from funasr import AutoModel
 
 
@@ -7,7 +9,9 @@ class FunAsr:
         self.model = AutoModel(model=model_dir,  vad_model="fsmn-vad", punc_model="ct-punc")
 
     def generate(self, input):
-        res = self.model.generate(input=f"{self.model.model_path}/example/{input}",
+        # res = self.model.generate(input=f"{self.model.model_path}/example/{input}",
+
+        res = self.model.generate(input=os.path.abspath(f"./files/{input}"),
             batch_size_s=300,
             hotword='魔搭')
         return res
--- app/asr_controller.py
+++ app/asr_controller.py
@@ -1,5 +1,7 @@
+import os.path
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI,File, UploadFile
 from asr import FunAsr
 
 app = FastAPI()
@@ -17,14 +19,36 @@ async def info():
     }
 
 
+funasrModel = None
+
+
 @app.get("/funasr/")
 async def asr(model: str, file: str):
-    # model=paraformer-zh
-    funasr = FunAsr(model)
+    global funasrModel
+    if funasrModel is None:
+        # model=paraformer-zh
+        funasrModel = FunAsr(model)
     # file=asr_example.wav
-    ret = funasr.generate(file)
+    ret = funasrModel.generate(file)
     return ret
 
 
+@app.post("/funasr/upload")
+async def upload(file: UploadFile = File(...)):
+    filename = file.filename
+    save_path = f'./files/'
+
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+
+    save_file = os.path.join(save_path, filename)
+
+    os_file = open(save_file, 'wb')
+    data = await file.read()
+    os_file.write(data)
+    os_file.close()
+
+    return {'msg': f'{filename}上传成功'}
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=9000)
+    uvicorn.run(app, host="0.0.0.0", port=9000)
